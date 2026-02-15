8b69de3b183b9ff2eca0576bda108da3c6389102
aa2673 <aa2673@kent.ac.uk>
2026-02-15 19:46:10 +0000

    add_member()

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,8 +1,8 @@
 def main():
     names, ranks, divisions, ids = init_database()
-    display_menu()
+    display_menu(names,ranks,divisions,ids)
     
-def display_menu():
+def display_menu(names,ranks,divisions,ids):
             print("Initialising System... \nInitialisation Complete. \nFleet Manager Online.")
             while True:
                 username = input("What is your full name: ").strip().title()
@@ -10,14 +10,14 @@ def display_menu():
                     print("Please enter a valid name")
                 else:
                     print("\nWelcome", username)
-                    print("\n---MENU--- \n1. Add Member \n2. Remove Member \n3. Update Rank \n4. Display Roster \n5. Search Crew \n6. Filter Crew by division \n7. Calculate Payroll \n8. Count Officers \n9. Exit ")
                     break
                 
     
             while True:
-                choice = input("\nSelect an option: ")
+                print("\n---MENU--- \n1. Add Member \n2. Remove Member \n3. Update Rank \n4. Display Roster \n5. Search Crew \n6. Filter Crew by division \n7. Calculate Payroll \n8. Count Officers \n9. Exit ")
+                choice = input("Select an option: ")
                 if choice == "1":
-                    add_member()
+                    add_member(names,ranks,divisions,ids)
                 elif choice == "2":
                     remove_member()
                 elif choice == "3":
@@ -39,11 +39,29 @@ def display_menu():
                     print("Invalid choice")
 def init_database():
     names = ["James T. Kirk", "Spock", "Jean-Luc Picard", "Data", "Kathryn Janeway"]
-    ranks = ["Captain", "Lt. Commander", "Captain", "Lt. Commander", "Captain"]
+    ranks = ["Captain", "Lieutenant Commander", "Captain", "Lieutenant Commander", "Captain"]
     divisions = ["Command", "Science", "Command", "Operations", "Command"]
     ids = ["SC 937-0176 CEC", "S179-276", "289-49-16-JLP", "034-756-319-SPD", "927-20-40-52-T"]
 
     return names, ranks, divisions, ids
+
+def add_member(names,ranks,divisions,ids):
+    validRanks = ["Fleet Admiral","Admiral", "Vice Admiral", "Rear Admiral", "Captain", "Commander", "Lieutenant Commander", "Lieutenant", "Ensign", "Cadet"]
+    validDivisions = ["Command", "Operations", "Science"]
+    while True:
+        newMemberName = input("\nNew members' name: ").strip().title()
+        newMemberRank = input("New members' rank: ").strip().title()
+        newMemberDivision = input("New members' division: ").strip().title()
+        newMemberId = input("New members' ID: ").upper()
+        if newMemberName != "" and newMemberRank in validRanks and newMemberDivision in validDivisions and newMemberId not in ids:
+            names.append(newMemberName)
+            ranks.append(newMemberRank)
+            divisions.append(newMemberDivision)
+            ids.append(newMemberId)
+            print("New member added.")
+            break
+        else:
+            print("Invalid please try again.")
 
 
 
