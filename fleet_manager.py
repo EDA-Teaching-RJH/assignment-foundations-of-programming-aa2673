11ce85926d91ef8761b1508ffaf36ee0451bb5ac
aa2673 <aa2673@kent.ac.uk>
2026-02-15 08:31:27 +0000

    init_database()

---
+++ fleet_manager.py
@@ -0,0 +1,7 @@
+def init_database()
+    Names = ["James T. Kirk", "Spock", "Jean-Luc Picard", "Data", "Kathryn Janeway"]
+    Ranks = ["Captain", "Lt. Commander", "Captain", "Lt. Commander", "Captain"]
+    Divisions = ["Command", "Science", "Command", "Operations", "Command"]
+    IDs = ["SC 937-0176 CEC", "S179-276", "289-49-16-JLP", "034-756-319-SPD", "927-20-40-52-T"]
+
+    return Names, Ranks, Divisions, IDs
