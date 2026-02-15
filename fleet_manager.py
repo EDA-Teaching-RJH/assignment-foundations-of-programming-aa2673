bbff6637d1aa4f5d74791e8ac163783069a2ce38
aa2673 <aa2673@kent.ac.uk>
2026-02-15 08:35:09 +0000

    main()

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,7 +1,13 @@
+def main()
+    
 def init_database()
-    Names = ["James T. Kirk", "Spock", "Jean-Luc Picard", "Data", "Kathryn Janeway"]
-    Ranks = ["Captain", "Lt. Commander", "Captain", "Lt. Commander", "Captain"]
-    Divisions = ["Command", "Science", "Command", "Operations", "Command"]
-    IDs = ["SC 937-0176 CEC", "S179-276", "289-49-16-JLP", "034-756-319-SPD", "927-20-40-52-T"]
+    names = ["James T. Kirk", "Spock", "Jean-Luc Picard", "Data", "Kathryn Janeway"]
+    ranks = ["Captain", "Lt. Commander", "Captain", "Lt. Commander", "Captain"]
+    divisions = ["Command", "Science", "Command", "Operations", "Command"]
+    ids = ["SC 937-0176 CEC", "S179-276", "289-49-16-JLP", "034-756-319-SPD", "927-20-40-52-T"]
 
-    return Names, Ranks, Divisions, IDs
+    return names, ranks, divisions, ids
+
+def 
+
+main()
